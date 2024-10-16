38e215e8e79f9773facb218264a2a8de4ec64ab8
anandasabhiram4 <anandasabhiram4@gmail.com>
2024-10-16 14:01:00 +0530

    DONE

--- test_ffmpeg_command.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# Run a simple FFmpeg command to get the version
-try:
-    result = subprocess.run(['/opt/homebrew/bin/ffmpeg', '-version'], capture_output=True, text=True)
-    print("FFmpeg version output:", result.stdout)
-except Exception as e:
-    print("Error running ffmpeg:", e)
